2c1a40dc813768575aa50b387d47f49d0d5a606a
project159085 <project159085@gmail.com>
2025-12-08 13:47:36 +0530

    Created Folder Structure and Auth Context

---
+++ frontend/src/authContext.jsx
@@ -0,0 +1,8 @@
+import React, { createContext, useState, useEffect, useContext } from 'react';
+
+const AuthContext = createContext();
+
+export const useAuth = () => {
+  return useContext(AuthContext);
+};
+
